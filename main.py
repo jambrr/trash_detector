4672a11ce68adca2b681809165e7caa1cbf21245
Louis James Weber louis.weber.001@student.uni.lu <lweber@access1.iris-cluster.uni.lux>
2024-11-25 19:24:34 +0100

    new project structure

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-from src.data.preprocessor import TacoPreprocessor
+from src.data.preprocessor import TacoPreprocessor, TrashnetPreprocessor
 from src.models.trainers import Yolov11Trainer
+import logging
+logger = logging.getLogger(__name__)
+
+# preprocessor = TacoPreprocessor("taco", "datasets/TACO", "datasets/TACO/data/annotations.json")
+preprocessor = TrashnetPreprocessor("trashnet", "datasets/trashnet", "datasets/trashnet/train/_annotations.coco.json",  "datasets/trashnet/valid/_annotations.coco.json")
+
+preprocessor.preprocess(preprocessor.train_df)
+
+print(preprocessor.augmented_df.head())
+
+print(len(preprocessor.train_df))
